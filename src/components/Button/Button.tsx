d52ad505191c15d00520f1bb4c0c372ba29c89ee
modem1 <monika.dembek@knowit.eu>
2023-12-03 14:58:19 +0100

    Test button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,9 @@
 import { PropsWithChildren } from "react";
 import { PrimaryButton, OutlinedButton, InvertedButton } from "./Button.style";
 
-type ButtonType = "primary" | "inverted" | "outlined";
+export type ButtonType = "primary" | "inverted" | "outlined";
 
-type ButtonProps = {
+export type ButtonProps = {
   type?: ButtonType;
   handleClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 };
@@ -17,25 +17,41 @@ function Button({
   switch (type) {
     case "primary":
       return (
-        <PrimaryButton {...delegated} onClick={handleClick}>
+        <PrimaryButton
+          {...delegated}
+          onClick={handleClick}
+          data-testid="primaryButton"
+        >
           {children}
         </PrimaryButton>
       );
     case "inverted":
       return (
-        <InvertedButton {...delegated} onClick={handleClick}>
+        <InvertedButton
+          {...delegated}
+          onClick={handleClick}
+          data-testid="invertedButton"
+        >
           {children}
         </InvertedButton>
       );
     case "outlined":
       return (
-        <OutlinedButton {...delegated} onClick={handleClick}>
+        <OutlinedButton
+          {...delegated}
+          onClick={handleClick}
+          data-testid="outlinedButton"
+        >
           {children}
         </OutlinedButton>
       );
     default:
       return (
-        <PrimaryButton {...delegated} onClick={handleClick}>
+        <PrimaryButton
+          {...delegated}
+          onClick={handleClick}
+          data-testid="primaryButton"
+        >
           {children}
         </PrimaryButton>
       );
--- src/helpers/fetcher.ts
+++ src/helpers/fetcher.ts
@@ -1,3 +1,11 @@
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+// const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-export default fetcher;
+// export default fetcher;
+
+export default async function fetcher<JSON = any>(
+  input: RequestInfo,
+  init?: RequestInit,
+): Promise<JSON> {
+  const res = await fetch(input, init);
+  return res.json();
+}
