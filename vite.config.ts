973ace001b52385498db935fd1451d0518a5a7bc
modem1 <monika.dembek@knowit.eu>
2024-03-23 18:13:47 +0100

    Prepare vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import react from "@vitejs/plugin-react";
+import packageJson from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    minify: true,
+    sourcemap: false,
+  },
+  define: {
+    APP_VERSION: JSON.stringify(packageJson.version),
+  },
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    react(),
+    splitVendorChunkPlugin(),
+    ViteImageOptimizer({
+      png: {
+        // https://sharp.pixelplumbing.com/api-output#png
+        quality: 100,
+      },
+      jpeg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 100,
+      },
+      jpg: {
+        // https://sharp.pixelplumbing.com/api-output#jpeg
+        quality: 100,
+      },
+    }),
+  ],
+});
